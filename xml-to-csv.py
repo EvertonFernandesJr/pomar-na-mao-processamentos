153ad53fc5b78013619defc4a61f7086f4da64b0
EvertonFernandesJr <evertonfernandesjr@gmail.com>
2025-06-08 15:13:30 -0300

    conversor completo

--- xml-to-csv.py
+++ xml-to-csv.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import csv
 import xml.etree.ElementTree as ET
 
 
@@ -12,21 +12,29 @@ documents = root.findall('kml:Document', ns)
 for document in documents:
   placemarks = document.findall("kml:Placemark", ns)
 
+dados = []
+
 for placemark in placemarks:
   name = placemark.find("kml:name",ns)
-  print(f'Nome: {name.text}')
+  tipo = ""
+  coordenadas = ""
   
   point = placemark.find('kml:Point', ns)
   if point is not None:
     coords = point.find('kml:coordinates', ns)
-    print(f'Tipo: Ponto')
-    print(f'Coordenadas {coords.text.strip()}')
+    tipo = "Ponto"
+    coordenadas = coords.text.strip()
   polygon = placemark.find('kml:Polygon', ns)
   if polygon is not None:
     coords = polygon.find('.//kml:coordinates', ns)
-    print('Tipo: Poligono')
-    print(f'Coordenadas {coords.text.strip()}')
-  print('---')
+    tipo = "Polígono"
+    coordenadas = coords.text.strip()
+  dados.append([name.text, tipo, coordenadas])
+
+with open("saida.csv", mode="w", newline="", encoding="utf-8") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Nome", "Tipo", "Coordenadas"])
+    writer.writerows(dados)
 
 
 
